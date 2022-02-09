c44eb390a23fd8d6d9bb58660cff4a4e8f5ebdae
kirill <turovkv@yandex.ru>
2022-02-09 18:19:10 +0300

    Refactor the code

--- hw_01/setup.py
+++ hw_01/setup.py
@@ -1,22 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="fibastprint",
+    name="astprintfib",
     version="0.1.0",
-    packages=['fibastprint'],
-    description="fibastprint",
+    packages=['astprintfib'],
     url="https://github.com/turovkv/python-hw/tree/main/hw_01",
     author="turovkv",
-    author_email="turovkv@yandex.ru",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-    ],
     install_requires=[
-        "networkx~=2.6.3",
-        "matplotlib~=3.5.1",
-        "pydot~=1.4.2",
+        "networkx==2.6.3",
+        "matplotlib==3.5.1",
+        "pydot==1.4.2",
     ],
 )
